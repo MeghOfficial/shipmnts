4315dcf3860da076ed99d0c851b1227e9258135c
MeghOffical <202301402@dau.ac.in>
2026-08-24 10:14:42 +0530

    Initial boilerplate setup

--- app/core/database.py
+++ app/core/database.py
@@ -1,24 +1,34 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
 
-from app.core.config import settings
-
-
-engine = create_engine(settings.DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-Base = declarative_base()
+# from app.core.config import settings
 
 
-def get_db():
-    db = SessionLocal()
+# engine = create_engine(settings.DATABASE_URL)
 
-    try:
-        yield db
-    finally:
-        db.close()
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# Base = declarative_base()
+
+
+# def get_db():
+#     db = SessionLocal()
+
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client["sde_round_db"]
